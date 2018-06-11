65d68662441ee2e9080b6c6ab774ec377d191a70
karthikprasadsm <kartik.scmp@gmail.com>
2018-06-11 17:01:10 +0530

    updated....

---
+++ com.qa.atm/src/main/java/com/qa/atmautomation/com/qa/atm/App.java
@@ -0,0 +1,38 @@
+package com.qa.atmautomation.com.qa.atm;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+
+
+   
+
+    	public static void main(String[] args) {
+    		// TODO Auto-generated method stub
+    		System.setProperty("webdriver.gecko.driver","./Drivers/geckodriver.exe");
+    		DesiredCapabilities capabilities=DesiredCapabilities.firefox();
+    		capabilities.setCapability("marionette", true);
+    		
+    		WebDriver driver = new FirefoxDriver();
+            //WebDriver driver = new FirefoxDriver();
+            driver.navigate().to("https://www.google.com");
+            driver.close();
+            
+            
+            System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
+            WebDriver driver1 = new ChromeDriver();
+            driver1.get("http://www.google.com");
+            driver1.close();
+            
+    	}
+
+    }
+
